0eaad86c65a51ce3797ff5337e62ae435cbabb51
hanbee <sonhanbi1002@gmail.com>
2020-08-23 17:05:42 +0900

    create custom cell and move to detail view

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 1600000, 80000000, 77000000, 120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,12 +22,19 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     // UITableViewDatasource에 포함된 필수 protocol
     // 총 몇 개의 row로 이루어 지는지
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return bountyList.count
     }
     
     // 어떤 셀로 보여줄건지
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
         
         return cell
     }
@@ -33,5 +43,13 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     // 클릭 시 어떻게 동작할지
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("--> \(indexPath.row)")
+        performSegue(withIdentifier: "showDetail", sender: nil)
     }
+}
+
+// Custom cell
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
 }
