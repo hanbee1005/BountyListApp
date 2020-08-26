d595bb65a0a6ffc3dc347c2b1455a99a0c04a5f9
hanbee <sonhanbi1002@gmail.com>
2020-08-26 21:51:54 +0900

    detail view animation 적용

--- BountyList/DetailViewController.swift
+++ BountyList/DetailViewController.swift
@@ -14,12 +14,40 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
     
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
+    
     var viewModel = DetailViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        showAnimation()
+    }
+    
+    // animation 준비하기
+    private func prepareAnimation() {
+        nameLabelCenterX.constant = view.bounds.width
+        bountyLabelCenterX.constant = view.bounds.width
+    }
+    
+    // animation 보여주기
+    private func showAnimation() {
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+        
+        // animation을 통해 layout update
+        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {self.view.layoutIfNeeded()}, completion: nil)
+        
+        // imgView 좌우로 뒤집기
+        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     func updateUI() {
