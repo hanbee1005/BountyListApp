d6b6b30bd257435c63366a13f638e0c5fbcf75f9
hanbee <sonhanbi1002@gmail.com>
2020-08-26 21:12:40 +0900

    CollectionView로 전환

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+class BountyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     // Model
     // - BountyInfo
     // > BountyInfo 만들기
@@ -44,34 +43,46 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         super.viewDidLoad()
     }
     
-    // UITableViewDatasource에 포함된 필수 protocol
-    // 총 몇 개의 row로 이루어 지는지
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // UICollectionViewDataSource
+    // 총 몇 개를 보여줄지
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfBountyInfoList
     }
     
-    // 어떤 셀로 보여줄건지
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
-            return UITableViewCell()
+    // 셀을 어떻게 구현할지
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
         }
         
-        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
         cell.update(info: bountyInfo)
-        
         return cell
     }
-    
-    // UITableViewDelegate에 포함된 protocol
-    // 클릭 시 어떻게 동작할지
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("--> \(indexPath.row)")
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+
+    // UICollectionViewDelegate
+    // 셀이 클릭 되었을 때 어떻게 할지
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("--> \(indexPath.item)")
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+
+    }
+        
+    // UICollectionViewDelegateFlowLayout
+    // 셀 사이즈를 계산 (목표: 다양한 디바이스에서 일관적인 디자인을 보여주기 위해)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 10 // item 사이의 거리
+        let textAreaHeight: CGFloat = 65 // 이름과 현상금 구역
+        
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2
+        let height: CGFloat = width * 10/7 + textAreaHeight
+        
+        return CGSize(width: width, height: height)
     }
 }
 
-// Custom cell
-class ListCell: UITableViewCell {
+// Custom Cell
+class GridCell: UICollectionViewCell {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
